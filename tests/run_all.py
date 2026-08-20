408b2933fc67586fb3cbd9f6f2567ca0bc0b683c
abacus <davide@isoardi.info>
2026-08-20 19:01:57 +0200

    test: aggiunge test_state_store.py a run_all.py

--- tests/run_all.py
+++ tests/run_all.py
@@ -17,6 +17,7 @@ test_files = [
     os.path.join(TESTS_DIR, "test_applog.py"),
     os.path.join(TESTS_DIR, "test_ops.py"),
     os.path.join(TESTS_DIR, "test_ansible_runner.py"),
+    os.path.join(TESTS_DIR, "test_state_store.py"),
 ]
 
 total_failed = 0
